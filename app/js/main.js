b4f44c594246d8e1f470e6910ff303a20dec87fd
Jared Palmer <palmdeezy>
2015-07-27 15:12:42 -0400

    lots of fun stuff

--- app/js/main.js
+++ app/js/main.js
@@ -1,315 +1,252 @@
-var initPhotoSwipeFromDOM = function(gallerySelector) {
+// jQuery(document)
+//   .ready(function ($) {
+//     var siteUrl = 'http://' + (document.location.hostname || document.location.host);
+//
+//     // Make sure that all clicked links that link to your internal website
+//     // don't just reload the page but execute a History.pushState call
+//     $(document)
+//       .delegate('a[href^="/"],a[href^="' + siteUrl + '"]', "click", function (e) {
+//
+//         e.preventDefault();
+//
+//         History.pushState(null, null, this.pathname);
+//       });
+//
+//     // Catch all History stateChange events
+//     History.Adapter.bind(window, 'statechange', function () {
+//       var State = History.getState();
+//
+//
+//       // Load the new state's URL via an Ajax Call
+//       $.get(State.url, function (data) {
+//         // Replace the "<title>" tag's content
+//         document.title = $(data)
+//           .find("title")
+//           .text();
+//
+//         // Replace the content of the main container (.content)
+//         // If you're using another div, you should change the selector
+//         $('body').animate({scrollTop: 0}, 400)
+//         $('.content')
+//           .velocity('transition.slideDownOut', {
+//             duration: 600
+//           })
+//           .promise()
+//           .done(function () {
+//             $('.content')
+//               .html($(data)
+//                 .find('.content'));
+//             $('.content').velocity('transition.slideUpIn', { stagger: 100, duration: 400, drag: true });
+//           });
+//       });
+//       // If you're using Google analytics, make sure the pageview is registered!
+//       // ga('send', 'pageview', {
+//       //     'page': State.url,
+//       //     'title': document.title
+//       // });
+//     });
+//   });
+(function ($) {
+    var $document = $(document);
 
-  // parse slide data (url, title, size ...) from DOM elements
-  // (children of gallerySelector)
-  var parseThumbnailElements = function(el) {
-    var thumbElements = el.childNodes,
-      numNodes = thumbElements.length,
-      items = [],
-      figureEl,
-      childElements,
-      linkEl,
-      size,
-      item;
-
-    for (var i = 0; i < numNodes; i++) {
-
-
-      figureEl = thumbElements[i]; // <figure> element
-
-      // include only element nodes
-      if (figureEl.nodeType !== 1) {
-        continue;
-      }
-
-      linkEl = figureEl.children[0]; // <a> element
-
-      size = linkEl.getAttribute('data-size').split('x');
-
-      // create slide object
-      item = {
-        src: linkEl.getAttribute('href'),
-        w: parseInt(size[0], 10),
-        h: parseInt(size[1], 10)
-      };
-
-
-
-      if (figureEl.children.length > 1) {
-        // <figcaption> content
-        item.title = figureEl.children[1].innerHTML;
-      }
-
-      if (linkEl.children.length > 0) {
-        // <img> thumbnail element, retrieving thumbnail url
-        item.msrc = linkEl.children[0].getAttribute('data-src');
-      }
-
-      item.el = figureEl; // save link to element for getThumbBoundsFn
-      items.push(item);
+    if (!History.enabled) {
+      return false;
     }
 
-    return items;
-  };
+    var root = History.getRootUrl();
 
-  // find nearest parent element
-  var closest = function closest(el, fn) {
-    return el && (fn(el) ? el : closest(el.parentNode, fn));
-  };
-
-  // triggers when user clicks on thumbnail
-  var onThumbnailsClick = function(e) {
-    e = e || window.event;
-    e.preventDefault ? e.preventDefault() : e.returnValue = false;
-
-    var eTarget = e.target || e.srcElement;
-
-    var clickedListItem = closest(eTarget, function(el) {
-      return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
-    });
-
-    if (!clickedListItem) {
-      return;
-    }
-
-
-    // find index of clicked item
-    var clickedGallery = clickedListItem.parentNode,
-      childNodes = clickedListItem.parentNode.childNodes,
-      numChildNodes = childNodes.length,
-      nodeIndex = 0,
-      index;
-
-    for (var i = 0; i < numChildNodes; i++) {
-      if (childNodes[i].nodeType !== 1) {
-        continue;
-      }
-
-      if (childNodes[i] === clickedListItem) {
-        index = nodeIndex;
-        break;
-      }
-      nodeIndex++;
-    }
-
-
-
-    if (index >= 0) {
-      openPhotoSwipe(index, clickedGallery);
-    }
-    return false;
-  };
-
-  // parse picture index and gallery index from URL (#&pid=1&gid=2)
-  var photoswipeParseHash = function() {
-    var hash = window.location.hash.substring(1),
-      params = {};
-
-    if (hash.length < 5) {
-      return params;
-    }
-
-    var vars = hash.split('&');
-    for (var i = 0; i < vars.length; i++) {
-      if (!vars[i]) {
-        continue;
-      }
-      var pair = vars[i].split('=');
-      if (pair.length < 2) {
-        continue;
-      }
-      params[pair[0]] = pair[1];
-    }
-
-    if (params.gid) {
-      params.gid = parseInt(params.gid, 10);
-    }
-
-    if (!params.hasOwnProperty('pid')) {
-      return params;
-    }
-    params.pid = parseInt(params.pid, 10);
-    return params;
-  };
-
-  var openPhotoSwipe = function(index, galleryElement, disableAnimation) {
-    var pswpElement = document.querySelectorAll('.pswp')[0],
-      gallery,
-      options,
-      items;
-
-    items = parseThumbnailElements(galleryElement);
-
-    // define options (if needed)
-    options = {
-      index: index,
-
-      // define gallery index (for URL)
-      galleryUID: galleryElement.getAttribute('data-pswp-uid'),
-
-      getThumbBoundsFn: function(index) {
-        // See Options -> getThumbBoundsFn section of docs for more info
-        var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
-          pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
-          rect = thumbnail.getBoundingClientRect();
-
-        return {
-          x: rect.left,
-          y: rect.top + pageYScroll,
-          w: rect.width
-        };
-      },
-
-      // history & focus options are disabled on CodePen
-      // remove these lines in real life:
-      history: true,
-      focus: true
-
-      //,
-      //  showHideOpacity:true
-
+    $.expr.filters.internal = function (elem) {
+      return (elem.hostname == window.location.hostname && /(\/|\.html)$/i.test(elem.pathname)) || false;
     };
 
-    if (disableAnimation) {
-      options.showAnimationDuration = 0;
+    function find_all($html, selector) {
+      return $html.filter(selector)
+        .add($html.find(selector));
     }
 
-    // Pass data to PhotoSwipe and initialize it
-    gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
-    gallery.init();
-  };
+    function parse_html(html) {
+      return $($.parseHTML(html, document, true));
+    }
 
-  // loop through all gallery elements and bind events
-  var galleryElements = document.querySelectorAll(gallerySelector);
+    function parse_response(html) {
+      var
+        head = /<head[^>]*>([\s\S]+)<\/head>/.exec(html),
+        body = /<body[^>]*>([\s\S]+)<\/body>/.exec(html),
 
-  for (var i = 0, l = galleryElements.length; i < l; i++) {
-    galleryElements[i].setAttribute('data-pswp-uid', i + 1);
-    galleryElements[i].onclick = onThumbnailsClick;
-  }
+        $head = head ? parse_html(head[1]) : $(),
+        $body = body ? parse_html(body[1]) : $(),
 
-  // Parse URL and open gallery if it contains #&pid=3&gid=1
-  var hashData = photoswipeParseHash();
-  if (hashData.pid > 0 && hashData.gid > 0) {
-    openPhotoSwipe(hashData.pid - 1, galleryElements[hashData.gid - 1], true);
-  }
-};
+        title = $.trim(find_all($head, 'title')
+          .last()
+          .html()),
+        $content = $.trim(find_all($body, '#content')
+          .first()
+          .html());
 
-// execute above function
-// $(function() {
-//
-//
-//
-//   // initPhotoSwipeFromDOM('.photoset');
-//
-//   // $('h1').velocity('transition.slideUpIn', { delay: 380, duration: 1000, drag: true });
-//   // $('.posts-list--item').velocity('transition.slideUpBigIn', { stagger: 70, delay: 450, duration:1000, drag: true });
-//   // $('.navbar-link').velocity('transition.slideDownIn', { stagger: 60, delay: 100, duration: 1000, drag: true });
-//   'use strict';
-//   var $body = $('html, body'),
-//     content = $('#main').smoothState({
-//         prefetch: true,
-//         pageCacheSize: 4,
-//         onStart: {
-//           duration: 250,
-//           render: function(url, $container) {
-//             // Add your CSS animation reversing class
-//             $('#main').addClass('is-exiting');
-//
-//             // Restart your animation
-//             smoothState.restartCSSAnimations();
-//
-//             // anything else
-//           }
-//         },
-//         onEnd: {
-//           duration: 0,
-//           render: function(url, $container, $content) {
-//             // Remove your CSS animation reversing class
-//             $('#main').removeClass('is-exiting');
-//
-//             // Inject the new content
-//             $container.html($content);
-//           }
-//         }
-//       }
-//     }).data('smoothState');
-// });
-//
-// $(function() {
-//
-//
-//
-// });
-
-;
-(function($) {
-  'use strict';
-  var $body = $('html, body'),
-    $main = $('#main'),
-    options = {
-      prefetch: true,
-      // pageCacheSize: 4,
-      onStart: {
-        duration: 600,
-        render: function(url, $container) {
-          $body.animate({
-            scrollTop: 0
-          });
-           $main.addClass('is-exiting');
-          // smoothState.restartCSSAnimations();
-          // $('h1').velocity('transition.slideUpOut', { delay: 380, duration: 1000, drag: true });
-          // $('.posts-list--item').velocity('transition.slideDownBigOut', { stagger: 70, delay: 450, duration:1000, drag: true });
-          // // $('.navbar-link').velocity('transition.fadeOut', { stagger: 70, delay: 100, duration: 1000, drag: true });
-          // $('p').velocity('transition.slideDownOut', { stagger: 60, delay: 100, duration: 1000, drag: true });
-          // $('nav').velocity('transition.slideUpOut', {  duration: 200});
-          // $('.container').velocity('transition.fadeOut', {  duration: 200 });
-          // $('.photos').velocity('transition.slideDownOut', {  duration: 200 });
-          // $('footer').velocity('transition.fadeOut', {  duration: 200, });
-          // $('#main').velocity('transition.slideDownOut', { duration: 1000 });
-          // $('.navbar-link').velocity('transition.slideUpOut', {  duration: 200, stagger:40, drag: true});
-          $('#content').velocity('transition.slideDownOut', {  duration: 600, stagger:40, drag: true});
-          $(".lazy").unveil(200, function() {
-            $(this).load(function() {
-              this.style.opacity = 1;
-            });
-          });
-          // smoothState.restartCSSAnimations();
-        }
-      },
-      onEnd: {
-        duration: 400,
-        render: function(url, $container, $content) {
-          $main.removeClass('is-exiting');
-          $main.html($content);
-          $body.css('cursor', 'auto');
-          $body.find('a').css('cursor', 'auto');
-          // $('h1').velocity('transition.slideUpIn', { delay: 380, duration: 1000, drag: true });
-          // $('#main').velocity('transition.slideUpIn', { duration: 1000 });
-          // $('.navbar-link').velocity('transition.slideDownIn', {  duration: 200});
-          // initPhotoSwipeFromDOM('.photoset');
-          $('#content').velocity('transition.slideUpIn', { duration: 400, stagger:20, drag: true});
-          $(".lazy").unveil(200, function() {
-            $(this).load(function() {
-              this.style.opacity = 1;
-            });
-          });
-
-          // $('nav').velocity('transition.slideDownIn', { duration: 200 });
-          // $('.container').velocity('transition.slideUpIn', { duration: 200 });
-          // $('footer').velocity('transition.slideUpIn', {  duration: 200 });
-          // $('.photos').velocity('transition.slideUpIn', { duration: 200});
-
-          // $('.posts-list--item').velocity('transition.slideUpBigIn', { stagger: 70, delay: 450, duration:1000, drag: true });
-          // // $('.navbar-link').velocity('transition.fadeIn', { stagger: 70, delay: 100, duration: 1000, drag: true });
-          // $('p').velocity('transition.slideUpIn', { stagger: 60, delay: 100, duration: 1000, drag: true });
-
-
-
-
-
-        }
+      return {
+        'title': title,
+        '$content': $content
       }
-    },
-    smoothState = $main.smoothState(options).data('smoothState');
+    }
 
-})(jQuery);
+    prefix = "-webkit-";
+
+    animateCntx = function () {
+      window.requestAnimationFrame(function () {
+        $canvas = $("#cntx-canvas");
+        scrollTop = $window.scrollTop();
+        animationValue = scrollTop - $canvas.position()
+          .top;
+        // console.log(animationValue);
+        yVal = animationValue / 10;
+        yVal = -yVal.toFixed(5);
+        transform = "translate3d(0px, " + yVal + "px, 0px)";
+        transform2 = "translate3d(0px, " + -yVal + "px, 0px)";
+        $canvas.children(".phone1")
+          .css(prefix + "transform", transform);
+        $canvas.children(".phone2")
+          .css(prefix + "transform", transform2);
+      });
+    }
+
+    $window = $(window);
+    $body = $('body');
+    windowHeight = $window.height();
+    windowWidth = $window.width();
+    bodyHeight = $("#content")
+      .height();
+    $window.scroll(0);
+
+    setupCanvas = function (element, animationFunction) {
+      $(element)
+        .bind('inview', function (event, visible) {
+          if (visible == true) {
+            scrollIntervalID = setInterval(animationFunction, 50);
+          } else {
+            scrollIntervalID = 0;
+          }
+        });
+    }
+
+    $document.ready(function () {
+
+      $document.on('click', 'a:internal', function (event) {
+        if (event.which == 2 || event.ctrlKey || event.metaKey) {
+          return true;
+        }
+
+        History.pushState(null, null, $(this)
+          .attr('href'));
+        event.preventDefault();
+
+        return false;
+      });
+
+
+      setupCanvas('#cntx-canvas', animateCntx);
+
+    });
+    $(window).load(function () {$window.scroll(0);});
+    $(window)
+      .on('statechange', function () {
+        var
+          url = History.getState()
+          .url,
+          rel = url.replace(root, '/');
+
+        $.get(rel)
+          .done(function (date) {
+            var response = parse_response(date);
+
+            if (!response.$content.length) {
+              document.location.href = url;
+
+              return false;
+            }
+
+            var $content = $('#content');
+
+
+            if (response.title.length) {
+              $('title')
+                .last()
+                .html(response.title);
+            }
+
+
+            $content
+              .velocity('transition.slideDownOut', {
+                duration: 400
+              })
+              .promise()
+              .done(function () {
+                setupCanvas('#cntx-canvas', animateCntx);
+                $content
+                  .velocity('transition.slideUpIn', {
+                    duration: 300
+                  })
+                  .html(response.$content)
+              });
+          })
+          .fail(function () {
+            document.location.href = url;
+
+            return false;
+          });
+      });
+
+
+
+
+
+    })(jQuery);
+
+  // prefix = "-webkit-";
+  //
+  // animateCntx = function () {
+  //   window.requestAnimationFrame(function () {
+  //     $canvas = $("#cntx-canvas");
+  //     scrollTop = $window.scrollTop();
+  //     animationValue = scrollTop - $canvas.position()
+  //       .top;
+  //     // console.log(animationValue);
+  //     yVal = animationValue / 10;
+  //     yVal = -yVal.toFixed(5);
+  //     transform = "translate3d(0px, " + yVal + "px, 0px)";
+  //     transform2 = "translate3d(0px, " + -yVal + "px, 0px)";
+  //     $canvas.children(".phone1")
+  //       .css(prefix + "transform", transform);
+  //     $canvas.children(".phone2")
+  //       .css(prefix + "transform", transform2);
+  //   });
+  // }
+  //
+  //
+  // $(document)
+  // .ready(function () {
+  //   $window = $(window);
+  //   $body = $('body');
+  //   windowHeight = $window.height();
+  //   windowWidth = $window.width();
+  //   bodyHeight = $("#content")
+  //     .height();
+  //   $window.scroll(0);
+  //
+  //
+  //   setupCanvas = function (element, animationFunction) {
+  //     $(element)
+  //       .bind('inview', function (event, visible) {
+  //         if (visible == true) {
+  //           scrollIntervalID = setInterval(animationFunction, 50);
+  //         } else {
+  //           scrollIntervalID = 0;
+  //         }
+  //       });
+  //   }
+  //
+  //
+  //   setupCanvas('#cntx-canvas', animateCntx);
+  // }); $(window)
+  // .load(function () {
+  //   $window.scroll(0);
+  // });
